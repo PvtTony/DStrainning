c2738c65470df5e541c8227c7250d06d82bf4c50
Pvt.Tony <yusongtao518@outlook.com>
2016-10-18 18:38:01 +0800

    edited linkedlist

--- linkedlist/list.cpp
+++ linkedlist/list.cpp
@@ -3,83 +3,124 @@ using namespace std;
 
 struct List
 {
-  int num;
-  List *next;
+	int num;
+	List *next;
 };
 
 List *CreateList(int size);
 void DisplayList(List *head);
-void InsertItem(List *head, int val, int pos);
-
+List *InsertItem(List *head, int val, int pos);
+List *RemoveItem(List *head, int pos);
 
 List *CreateList(int size)
 {
-  List *p = NULL;
-  List *q = NULL;
-  List *head = NULL;
-  for (int i = 0; i < size; i++)
-  {
-    p = new List;
-    cin >> p->num;
-    if (head == NULL)
-      {
-	head = p;
-      }
-    else
-      {
-	q->next = p;
-      }
-    q = p;
-  }
-    if (head != NULL)
-      {
-	q->next = NULL;
-      }
+	List *p = NULL;
+	List *q = NULL;
+	List *head = NULL;
+	for (int i = 0; i < size; i++)
+	{
+		p = new List;
+		cin >> p->num;
+		if (head == NULL)
+		{
+			head = p;
+		}
+		else
+		{
+			q->next = p;
+		}
+		q = p;
+	}
+	if (head != NULL)
+	{
+		q->next = NULL;
+	}
 
-    return head;
+	return head;
 }
 
 void DisplayList(List *head)
 {
-  while(head != NULL)
-    {
-      cout << head->num;
-      head = head->next;
-      if (head != NULL)
+	while (head != NULL)
 	{
-	  cout << "->";
+		cout << head->num;
+		head = head->next;
+		if (head != NULL)
+		{
+			cout << "->";
+		}
 	}
-    }
-  cout << endl;
+	cout << endl;
 }
 
-void InsertItem(List *head, int val, int pos)
+List *InsertItem(List *head, int val, int pos)
 {
-  List *item = new List;
-  item->num = val;
-  List *temp_b4 = head;
-  while(--pos != 0)
-    {
-      temp_b4 = temp_b4->next;
-    }
-  List *temp_after = temp_b4->next;
-  item->next = temp_after;
-  temp_b4->next = item;
-  
+	List *item = new List;
+	item->num = val;
+	if (pos == 0)
+	{
+		//List *temp = head;
+		item->next = head;
+		head = item;
+//		head = temp;
+		return head;
+	}
+	List *temp_b4 = head;
+	while (--pos != 0)
+	{
+		temp_b4 = temp_b4->next;
+	}
+	List *temp_after = temp_b4->next;
+	item->next = temp_after;
+	temp_b4->next = item;
+	return head;
+
+}
+
+List *RemoveItem(List *head, int pos)
+{
+	if (head == NULL) return head;
+	if (pos == 0)
+	{
+		return head->next;
+	}
+	List *prev = head;
+	while (--pos != 0)
+	{
+		prev = prev->next;
+	}
+	List *del = prev->next, *after = del->next;
+	prev->next = after;
+	delete del;
+	return head;
 }
 
 int main(void)
 {
-  //List *CreateList(int size);
-  //void DisplayList(List *head);
-  List *testlist = new List;
-  int size;
-  cin >> size;
-  testlist = CreateList(size);
-  DisplayList(testlist);
-  cout << "Insert a element at pos 3" << endl;
-  InsertItem(testlist, 30, 3);
-  DisplayList(testlist);
-  delete testlist;
-  return 0;
+	//List *CreateList(int size);
+	//void DisplayList(List *head);
+	List *testlist = new List;
+	int size;
+	cout << "Size of the linkedlist" << endl;
+	cin >> size;
+	testlist = CreateList(size);
+	DisplayList(testlist);
+	cout << "Insert an element" << endl;
+	int insert_num, insert_pos;
+	cout << "pos: ";
+	cin >> insert_pos;
+	cout << "num: ";
+	cin >> insert_num;
+	cout << endl;
+	testlist = InsertItem(testlist, insert_num, insert_pos);
+	DisplayList(testlist);
+	cout << "Remove an item" << endl;
+	int remove_item;
+	cout << "pos: ";
+	cin >> remove_item;
+	cout << endl;
+	testlist = RemoveItem(testlist, remove_item);
+	DisplayList(testlist);
+	delete testlist;
+	return 0;
 }
